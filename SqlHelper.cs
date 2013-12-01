7e195fa00ea5b771a0f55b6e3b2b30533f6b8189
vipyeshuai <836246914@qq.com>
2013-12-01 09:22:01 +0800

    yeshuai

---
+++ SqlHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace HSMS.DAL
+{
+    class SqlHelper
+    {
+        private static string str = ConfigurationManager.ConnectionStrings["dbstr"].ConnectionString;
+
+        public static int ExcuteNonQuery(string sql,params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText=sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static object ExecuteScalar(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public static DataTable ExecuteDataTable(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataSet dataset = new DataSet();
+                    adapter.Fill(dataset);
+                    return dataset.Tables[0];
+                }
+            }
+        }
+
+        public static object FromDbValue(object value)
+        {
+            if (value == DBNull.Value)
+                return null;
+            else
+                return value;
+        }
+        public static object ToDbValue(object value)
+        {
+            if (value == null)
+                return DBNull.Value;
+            else
+                return value;
+        }
+    }
+}
